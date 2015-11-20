53d0bf02e05c146d582c566b7e2ba9b21d698662
James Lee <james@aspirationhosting.com>
2015-11-20 16:36:07 +0800

    Update filepermission.php

--- filepermission.php
+++ filepermission.php
@@ -1,4 +1,4 @@
 <?php
-`find . -type d -print0 | xargs -0 chmod 0755 && find . -type f -print0 | xargs -0 chmod 0644`;
+`find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {} \;`;
 echo "File Permissions All Set!";
 ?>
