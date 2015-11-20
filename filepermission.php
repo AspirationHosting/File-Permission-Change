cc0eb1eacb7d95ac067b39c5c3a6d6f582787e13
James Lee <james@aspirationhosting.com>
2015-11-20 16:48:20 +0800

    Update filepermission.php

--- filepermission.php
+++ filepermission.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * File Permission Change
+ * By Aspiration Hosting
+ * 
+ * GitHub URL: https://github.com/AspirationHosting/File-Permission-Change
+ */
 `find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {} \;`;
 echo "File Permissions All Set!";
 ?>
